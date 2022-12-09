0556430344e764f4dd8bfeb882a01c61a81c1e81
Davey Jacobson <daveyjake21@gmail.com>
2022-12-09 11:33:15 -0600

    Update and rename source/index.d.ts to index.d.ts
    Corrected according to Typescript specifications.

--- source/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { RouteRecordRaw } from 'vue-router';
-import { VueBodyClass } from 'vue-body-class';
-
-declare type VueBodyClass = InstanceType<typeof VueBodyClass>;
-declare function VueBodyClass( routes: RouteRecordRaw[] ) : VueBodyClass;
